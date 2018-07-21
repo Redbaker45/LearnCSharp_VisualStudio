9b18b12f959c2b754903bc4fa59385e810233b77
Morten <mkirkebaek@hotmail.com>
2018-07-21 16:55:00 +0200

    Add files via upload

---
+++ StatictPropertiesProgram.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace ConsoleStaticProperties
+{
+    class SavingsAcount
+    {
+        public double currBalance;
+
+        private static double currInterestRate = 0.04;
+
+        public static double interestRate
+        {
+            get { return currInterestRate; }
+            set { currInterestRate = value; }
+        }
+
+        public SavingsAcount(){}
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SavingsAcount savingsAccount = new SavingsAcount();
+            savingsAccount.currBalance = 0.05;
+
+            Console.WriteLine(savingsAccount.currBalance);
+            Console.Read();
+        }
+    }
+}
